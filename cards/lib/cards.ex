e4f40aba507d1786c4908027a9b175e704eb07fc
Taylor Fraley <tfraley@clearent.com>
2019-07-31 13:28:10 -0400

    add contains?

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,9 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["ace", "two", "three"]
+    ["ace", "two", "three", "four", "five"]
   end
 
   def shuffle(deck) do 
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do 
+    Enum.member?(deck, card)
   end
 end
