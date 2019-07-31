da6e9794d3f1b3a34cc5254c08c17c3166fd7ef5
Taylor Fraley <tfraley@clearent.com>
2019-07-31 13:39:23 -0400

    add suits to create_deck

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,11 @@
 defmodule Cards do
   def create_deck do
-    ["ace", "two", "three", "four", "five"]
+    values = ["ace", "two", "three", "four", "five"]
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do 
